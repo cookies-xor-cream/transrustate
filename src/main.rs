fdf756e4808b2d0360ea05ffbf1ee68ca5f7f1f4
cookies-xor-cream <aryageza@gmail.com>
2022-07-27 08:45:35 +0800

    iterates over text in tables

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use reqwest;
+use scraper::Html;
 // use scraper::Html;
 
-fn get_conjugation_tables() -> Vec<String> {
+fn get_conjugation_tables() -> Vec<Html> {
     let response = reqwest::blocking::get(
         "https://www.wordreference.com/conj/frverbs.aspx?v=saper",
     )
@@ -10,12 +11,29 @@ fn get_conjugation_tables() -> Vec<String> {
         .unwrap();
 
     let document = scraper::Html::parse_document(&response);
-    let table_queries = scraper::Selector::parse("table.neoConj > tbody").unwrap();
-    let tables = document.select(&table_queries).map(|x| x.inner_html());
+    let table_query = scraper::Selector::parse("table.neoConj").unwrap();
+    let tables = document.select(&table_query).map(|x| scraper::Html::parse_fragment(&x.html()));
 
-    tables.collect::<Vec<String>>()
+    tables.collect::<Vec<Html>>()
+}
+
+fn extract_conjugations_from_table(table: &Html) {
+    let row_query = scraper::Selector::parse("tr").unwrap();
+
+    let rows = table.select(&row_query);
+    for row in rows {
+        for cell in row.text() {
+            println!("cell: {:#?}", cell);
+        }
+    }
 }
 
 fn main() {
-    println!("{:#?}", get_conjugation_tables()[0]); // gets the first conjugation table
+    let conjugation_tables = get_conjugation_tables();
+    for conjugation_table in conjugation_tables {
+        println!("Conjugation Table:");
+        extract_conjugations_from_table(&conjugation_table);
+
+        println!("");
+    }
 }
