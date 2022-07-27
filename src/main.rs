4d66667ac3ce7e5d7389cc62842ec6a7d523564b
cookies-xor-cream <aryageza@gmail.com>
2022-07-27 16:13:53 +0800

    Restructured program to be more semantic

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,96 @@
 use reqwest;
-use scraper::Html;
+use scraper::{Html};
 
-struct ConjugationTable<'a> {
-    title: String,
-    items: Vec<Vec<&'a str>>,
+struct ConjugationTable {
+    tense: String,
+    conjugations: Vec<Vec<String>>,
 }
 
-fn scrape_conjugation_tables() -> Vec<Html> {
-    let response = reqwest::blocking::get(
-        "https://www.wordreference.com/conj/frverbs.aspx?v=saper",
-    )
-        .unwrap()
-        .text()
-        .unwrap();
-
-    let document = scraper::Html::parse_document(&response);
-    let table_query = scraper::Selector::parse("table.neoConj").unwrap();
-    let tables = document.select(&table_query).map(|x| scraper::Html::parse_fragment(&x.html()));
-
-    tables.collect::<Vec<Html>>()
-}
-
-fn extract_conjugations_from_table(table: &Html) -> ConjugationTable {
-    let mut conjugation_table = ConjugationTable {
-        title: "".to_string(),
-        items: Vec::new(),
-    };
-    
-    let row_query = scraper::Selector::parse("tr").unwrap();
-    let rows = table.select(&row_query);
-
-    // flatten all text in rows
-    let cell_values = rows.map(|row| {
-        row.text().collect::<Vec<&str>>()
-    })
-        .into_iter()
-        .flatten()
-        .collect::<Vec<&str>>();
-
-    conjugation_table.title = cell_values[0].to_string();
-
-    // skip header text
-    let mut text_iter = cell_values.iter().skip(2);
-    
-    // get pronoun/conjugation pairs remaining
-    let mut conjugations: Vec<Vec<&str>> = Vec::new();
-    loop {
-        if let Some(pronoun) = text_iter.next() {
-            if let Some(conjugation) = text_iter.next() {
-                conjugations.push(vec![pronoun, conjugation]);
+impl ConjugationTable {
+    fn new(cell_values: Vec<&str>) -> ConjugationTable {
+        // skip header text
+        let mut text_iter = cell_values.iter().skip(2);
+        
+        // get pronoun/conjugation pairs remaining
+        let mut conjugations: Vec<Vec<String>> = Vec::new();
+        loop {
+            if let Some(pronoun) = text_iter.next() {
+                if let Some(conjugation) = text_iter.next() {
+                    conjugations.push(vec![pronoun.to_string(), conjugation.to_string()]);
+                } else {
+                    break;
+                }
             } else {
                 break;
             }
-        } else {
-            break;
+        }
+
+        let tense = cell_values[0].to_string();
+
+        ConjugationTable {
+            tense: tense,
+            conjugations: conjugations,
+        }
+    }
+}
+
+struct VerbConjugations {
+    conjugation_tables: Vec<ConjugationTable>,
+}
+
+impl VerbConjugations {
+    fn new() -> VerbConjugations {
+        VerbConjugations {
+            conjugation_tables: Vec::new(),
         }
     }
 
-    conjugation_table.items = conjugations;
-    conjugation_table
-}
+    fn scrape_conjugation_tables(&self) -> Vec<Html> {
+        let response = reqwest::blocking::get(
+            "https://www.wordreference.com/conj/frverbs.aspx?v=saper",
+        )
+            .unwrap()
+            .text()
+            .unwrap();
 
-fn get_conjugation_tables() {
-    let scraped_tables = scrape_conjugation_tables();
-    for scraped_table in scraped_tables {
-        let table = extract_conjugations_from_table(&scraped_table);
+        let document = scraper::Html::parse_document(&response);
+        let table_query = scraper::Selector::parse("table.neoConj").unwrap();
+        let tables = document
+            .select(&table_query)
+            .map(|x| scraper::Html::parse_fragment(&x.html()));
+
+        tables.collect::<Vec<Html>>()
+    }
+
+    fn extract_conjugations_from_table(&mut self, table: Html) {        
+        let row_query = scraper::Selector::parse("tr").unwrap();
+        let rows = table.select(&row_query);
+
+        // flatten all text in rows
+        let cell_values = rows.map(|row| {
+            row.text().collect::<Vec<&str>>()
+        })
+            .into_iter()
+            .flatten()
+            .collect::<Vec<&str>>();
+
+        self.conjugation_tables.push(ConjugationTable::new(cell_values));
+    }
+
+    pub fn get_conjugation_tables() -> VerbConjugations {
+        let mut verb_conjugations = VerbConjugations::new();
+        let tables = verb_conjugations.scrape_conjugation_tables();
+        for table in tables {
+            verb_conjugations.extract_conjugations_from_table(table);
+        }
+
+        verb_conjugations
     }
 }
 
 fn main() {
-    get_conjugation_tables();
+    let verb_conjugations = VerbConjugations::get_conjugation_tables();
+    for table in verb_conjugations.conjugation_tables {
+        println!("{} {:?}", table.tense, table.conjugations);
+    }
 }
